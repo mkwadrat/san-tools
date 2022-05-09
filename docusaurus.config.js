fe947618bf30fe7ca6a68f1ccd96f8baa022e0ea
mkwadrat <m2markiewicz@gmail.com>
2022-05-09 10:28:29 +0200

    ver 0.0.2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,19 +33,14 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/mkwadrat/san-tools/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/mkwadrat/san-tools/',
-        },
+        blog: false,
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
